2226978bf52c04ab5891c1d22129f2716c15f60f
Norman Rusch <norman.rusch@moccu.com>
2018-02-15 13:13:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Penthouse Service
+
+A service to create criticalpath css using [penthouse](https://github.com/pocketjoso/penthouse).
+
+## Run service
+
+Install all dependencies using `npm install`. Afterward you can start the
+service using `npm run service`.
---
+++ index.js
@@ -0,0 +1,45 @@
+const
+	express = require('express'),
+	fetch = require('node-fetch'),
+	penthouse = require('penthouse'),
+	server = express(),
+
+	SERVER_PORT = 3000,
+	SERVER_PATH = '/'
+;
+
+
+async function fetchCss(css) {
+	return fetch(css).then(res => {
+		return res.text()
+	});
+}
+
+async function fetchCritical(url, cssString) {
+	return penthouse({url, cssString});
+}
+
+async function handleRequest(request, response) {
+	const {url, css} = request.query;
+
+	if (!css || !url) {
+		response.status(400).send('Missing correct params (url, css).');
+		return;
+	}
+
+	try {
+		const
+			cssString = await fetchCss(css),
+			critical = await fetchCritical(url, cssString)
+		;
+
+		response.send(critical);
+	} catch(error) {
+		console.error(error);
+		response.status(500).send('Error');
+	}
+}
+
+server
+	.get(SERVER_PATH, handleRequest)
+	.listen(SERVER_PORT, () => console.log('Listening on port 3000'));
